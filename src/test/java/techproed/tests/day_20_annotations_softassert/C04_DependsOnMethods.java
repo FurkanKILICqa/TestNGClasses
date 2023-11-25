a7e3dced0629b128bd43d6307d8f581489b4caaf
Furkan Kılıc <fkrx928@gmail.com>
2023-11-25 22:27:25 +0300

    my firts class

--- src/test/java/techproed/tests/day_20_annotations_softassert/C04_DependsOnMethods.java
+++ src/test/java/techproed/tests/day_20_annotations_softassert/C04_DependsOnMethods.java
@@ -3,11 +3,18 @@ package techproed.tests.day_20_annotations_softassert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class C04_DependsOnMethods {
 
+    //TestNG de tum  @Test metotlar bitbirlerinden bagimsiz olarak calisirlar
+    //    Eger methodlari bagimli hale getirmek istersem, dependsOnMethods parametresi kullanilir
+    //    Burdaki ornekde, test02 methodu test01 methoduna bagimlidir
+    //    test02 den once test01 calisir
+    //    Eger test01 basarili ise test02 calisir.
+    //    Eget test01 basarisiz ise, test02 IGNORE edilir
     WebDriver driver;
 
     @BeforeClass
@@ -15,5 +22,17 @@ public class C04_DependsOnMethods {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+
+    @Test
+    public void test01() {
+
+
+
+
+
+
+
     }
 }
