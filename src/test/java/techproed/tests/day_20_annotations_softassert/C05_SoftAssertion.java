a04de8f691d2eee1795332fdeb5d6c30107c7b63
Furkan Kılıc <fkrx928@gmail.com>
2023-11-25 22:54:35 +0300

    my firts class

--- src/test/java/techproed/tests/day_20_annotations_softassert/C05_SoftAssertion.java
+++ src/test/java/techproed/tests/day_20_annotations_softassert/C05_SoftAssertion.java
@@ -1,4 +1,14 @@
 package techproed.tests.day_20_annotations_softassert;
 
+import org.testng.annotations.Test;
+
 public class C05_SoftAssertion {
+
+
+    @Test
+    public void softAssertionTest() {
+
+
+
+    }
 }
