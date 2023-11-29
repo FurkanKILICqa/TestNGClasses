199b104e5cee16cb5bbe8cd31cd583ddd29d81f8
Furkan Kılıc <fkrx928@gmail.com>
2023-11-29 11:52:12 +0300

    my first class

---
+++ src/test/java/techproed/utilities/ExcelReader.java
@@ -0,0 +1,26 @@
+package techproed.utilities;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ExcelReader {
+
+
+    Workbook workbook;
+
+
+    Sheet sheet;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
