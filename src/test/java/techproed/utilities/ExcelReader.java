e7abf25634975b9a5afc035fed263cfb9106de03
Furkan Kılıc <fkrx928@gmail.com>
2023-11-29 12:01:55 +0300

    my first class

--- src/test/java/techproed/utilities/ExcelReader.java
+++ src/test/java/techproed/utilities/ExcelReader.java
@@ -2,6 +2,10 @@ package techproed.utilities;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ExcelReader {
 
@@ -11,9 +15,20 @@ public class ExcelReader {
 
     Sheet sheet;
 
+    //Bu class dan obje oluşturulmasının amaco excel dosyası okumaktır
+    //
+
+    public ExcelReader (String dosyaYolu,String sayfaIsmi){
 
 
-
+        try {
+            FileInputStream fis = new FileInputStream(dosyaYolu);
+            workbook=WorkbookFactory.create(fis);
+            workbook.getSheet(sayfaIsmi);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 
