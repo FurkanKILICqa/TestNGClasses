985d23a46729fc3ce920534b52f41858e107849d
Furkan Kılıc <fkrx928@gmail.com>
2023-11-29 13:41:58 +0300

    my first class

--- src/test/java/techproed/tests/day_23_excel/C03_BlueRentalCarExcelTest.java
+++ src/test/java/techproed/tests/day_23_excel/C03_BlueRentalCarExcelTest.java
@@ -1,5 +1,6 @@
 package techproed.tests.day_23_excel;
 
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import techproed.pages.BlueRentalPage;
 import techproed.utilities.ConfigReader;
@@ -34,15 +35,26 @@ public class C03_BlueRentalCarExcelTest {
         String pageName="customer_info";
         ExcelReader excelReader=new ExcelReader(classPath,pageName);
 
-        for (int i = 1; i <= excelReader.rowCount(); i++) {
-
+        for (int i = 1; i <= 7; i++) {//Burada excelReader.rowCount() bu şekilde yazmamızın sebebi pc de excel
+            // kurulu olmadığından dolayıdır Nulpointer hatası alırız excel olmadığı için olmayan
+            // bütün sütunları da almaya çalışır
 
           String email =  excelReader.getCellData(i,0);
           String password = excelReader.getCellData(i,1);
 
             System.out.println(email+" "+password);
 
+            blueRentalPage.loginButton.click();
+            blueRentalPage.email.sendKeys(email);
+            blueRentalPage.password.sendKeys(password, Keys.ENTER);
+            blueRentalPage.loginVerify.click();
+
+
+            break;
+
         }
+
+
 
 
 
